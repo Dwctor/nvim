4ad36f76c3f54719a02ea00eadd70c60418b74b9
Kael Soares Augusto <kaelsa@hotmail.com.br>
2023-09-06 14:34:44 -0300

    Added descriptions to each command with whichkey!

--- lua/Kvim/after.lua
+++ lua/Kvim/after.lua
@@ -1,9 +1,9 @@
 -- Telescope
 local telescope = require('telescope.builtin')
-vim.keymap.set('n', '<leader>sf', telescope.find_files, {})
-vim.keymap.set('n', '<leader>sn', telescope.find_files, {})
-vim.keymap.set('n', '<leader>sg', telescope.git_files, {})
-vim.keymap.set('n', '<leader>st', telescope.live_grep, {})
+vim.keymap.set('n', '<leader>sf', telescope.find_files, {desc = "Search File"})
+vim.keymap.set('n', '<leader>sn', telescope.find_files, {desc = "Search Navigate (File)"})
+vim.keymap.set('n', '<leader>sg', telescope.git_files, {desc = "Search GitFiles"})
+vim.keymap.set('n', '<leader>st', telescope.live_grep, {desc = "Search Text"})
 
 -- UndoTree
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
@@ -13,15 +13,14 @@ vim.opt.termguicolors = true
 vim.cmd.colorscheme('catppuccin')
 
 -- Fugitive
-vim.keymap.set("n", "<leader>g", vim.cmd.Git)
+vim.keymap.set("n", "<leader>g", vim.cmd.Git, {desc = "Fugitive Git"})
 
 -- FileTree
-vim.keymap.set("n", "<leader>e", ':NvimTreeToggle<Enter>', {})
-vim.keymap.set("n", "<leader>n", ':NvimTreeToggle<Enter>', {})
+vim.keymap.set("n", "<leader>e", ':NvimTreeToggle<Enter>', {desc = "Navigate (NVimTree)"})
+vim.keymap.set("n", "<leader>n", ':NvimTreeToggle<Enter>', {desc = "Navigate (NVimTree)"})
 
 -- Buffers
-vim.keymap.set('n', "<leader>tn", '<Cmd>BufferNext<CR>', {})
-vim.keymap.set('n', "<leader>tp", '<Cmd>BufferPrevious<CR>', {})
-vim.keymap.set('n', "<leader>x", '<Cmd>BufferClose<CR>', {})
-vim.keymap.set('n', "<leader>ts", '<Cmd>BufferPick<CR>', {})
-vim.keymap.set('n', "<leader>sb", '<Cmd>BufferPick<CR>', {})
+vim.keymap.set('n', "<leader>tn", '<Cmd>BufferNext<CR>', {desc = "Tab Next"})
+vim.keymap.set('n', "<leader>tp", '<Cmd>BufferPrevious<CR>', {desc = "Tab Previous"})
+vim.keymap.set('n', "<leader>x", '<Cmd>BufferClose<CR>', {desc = "Close Buffer"})
+vim.keymap.set('n', "<leader>ts", '<Cmd>BufferPick<CR>', {desc = "Jump to Buffer"})
