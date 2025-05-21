fabe8e0b024986316654c9a3d4bfdedd3b9b9753
Kael Soares Augusto <kaelsa@hotmail.com.br>
2025-05-21 15:12:48 -0300

    UpToDate.

--- lua/Kvim/config.lua
+++ lua/Kvim/config.lua
@@ -5,6 +5,17 @@ local lsp = require('lsp-zero').preset({
 	},
 })
 
+-- Toggle warnings
+vim.api.nvim_create_user_command("DiagnosticToggle", function()
+	local config = vim.diagnostic.config
+	local vt = config().virtual_text
+	config {
+		virtual_text = not vt,
+		underline = not vt,
+		signs = not vt,
+	}
+end, { desc = "toggle diagnostic" })
+
 lsp.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
